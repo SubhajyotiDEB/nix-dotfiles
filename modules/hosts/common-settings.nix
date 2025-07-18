a7bec1b49e0decaefdd12370426930483b33baf9
Raven <143816236+SubhajyotiDEB@users.noreply.github.com>
2025-07-18 13:43:29 +0000

    Add files via upload

---
+++ modules/hosts/common-settings.nix
@@ -0,0 +1,30 @@
+{ lib, config, ... }:
+
+{
+  options.common-settings = {
+    enable = lib.mkEnableOption "common settings that can be utilized for multiple hosts";
+    gc.options = lib.mkOption {
+      description = "additional options for gc";
+    };
+    flake = lib.mkOption {
+      description = "path to the flake to use";
+    };
+  };
+
+  config = lib.mkIf config.common-settings.enable {
+    services.printing.enable = true;
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = true;
+    };
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = config.common-settings.gc.options;
+      flake = config.common-settings.flake;
+    };
+  };
+}
