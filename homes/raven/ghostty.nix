f8a5adf2a2aaa84340db0ebe0e80e6168e628bb8
Raven <143816236+SubhajyotiDEB@users.noreply.github.com>
2025-07-18 13:40:08 +0000

    Add files via upload

---
+++ homes/raven/ghostty.nix
@@ -0,0 +1,13 @@
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      command = "fish";
+      shell-integration = "fish";
+      window-decoration = "server";
+      window-padding-x = 10;
+      font-family = "Maple Mono NF";
+      font-size = 14;
+    };
+  };
+}
