6318934cd812a1ce80ef98eec7844e72a1a1ee1e
SubhajyotiDEB <mrdeadlock8@gmail.com>
2025-11-28 16:46:20 +0530

    2025-11-28 16:46:20

--- homes/raven/default.nix
+++ homes/raven/default.nix
@@ -1,4 +1,4 @@
-  { pkgs, pkgs-librewolf, ... }:
+{ pkgs, pkgs-librewolf, ... }:
 
 {
   # General home stuff.
@@ -6,49 +6,48 @@
   home.homeDirectory = "/home/raven";
   home.stateVersion = "25.05"; # DO NOT CHANGE!
   home.packages = [
-       (pkgs.catppuccin-kde.override {
+    (pkgs.catppuccin-kde.override {
       flavour = [ "mocha" ];
       accents = [ "mauve" ];
       winDecStyles = [ "classic" ];
     })
-       pkgs.legcord
-       pkgs-librewolf.librewolf
-       pkgs.ghostty
-       pkgs.zapzap
-       pkgs.duckdb
-       pkgs.maple-mono.NF
-       pkgs.nerd-fonts.jetbrains-mono
-       pkgs.kdePackages.kconfig
-       pkgs.kdePackages.karousel
-       pkgs.kdePackages.kde-gtk-config
-       pkgs.libreoffice-qt-fresh
-       pkgs.signal-desktop
-       pkgs.unzip
-       pkgs.unrar
-       pkgs.zed-editor-fhs
-       pkgs.qbittorrent
-       pkgs.catppuccinifier-cli
-       pkgs.gsettings-desktop-schemas
-       pkgs.youtube-music
-       pkgs.obsidian
-       pkgs.bitwarden-desktop
-       pkgs.uv
-       pkgs.git
-       pkgs.git-lfs
-       pkgs.steam
-       pkgs.bottles
+    pkgs.legcord
+    pkgs-librewolf.librewolf
+    pkgs.zapzap
+    pkgs.duckdb
+    pkgs.maple-mono.NF
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.kdePackages.kconfig
+    pkgs.kdePackages.karousel
+    pkgs.kdePackages.kde-gtk-config
+    pkgs.libreoffice-qt-fresh
+    pkgs.signal-desktop
+    pkgs.unzip
+    pkgs.unrar
+    pkgs.zed-editor-fhs
+    pkgs.qbittorrent
+    pkgs.catppuccinifier-cli
+    pkgs.gsettings-desktop-schemas
+    pkgs.youtube-music
+    pkgs.obsidian
+    pkgs.bitwarden-desktop
+    pkgs.uv
+    pkgs.git
+    pkgs.git-lfs
+    pkgs.steam
+    pkgs.bottles
 
-   #LSP pkgs
-      pkgs.fish-lsp
-      pkgs.nixd
-      pkgs.nix-search-tv
-      pkgs.nixfmt-rfc-style
-      pkgs.nix-output-monitor
-      pkgs.simple-completion-language-server
-      pkgs.taplo
-      pkgs.vscode-langservers-extracted
-      pkgs.yaml-language-server
-       ];
+    #LSP pkgs
+    pkgs.fish-lsp
+    pkgs.nixd
+    pkgs.nix-search-tv
+    pkgs.nixfmt-rfc-style
+    pkgs.nix-output-monitor
+    pkgs.simple-completion-language-server
+    pkgs.taplo
+    pkgs.vscode-langservers-extracted
+    pkgs.yaml-language-server
+  ];
 
   # Fontconfig stuff.
   fonts.fontconfig.enable = true;
@@ -59,20 +58,23 @@
   # Allow unfree.
   nixpkgs.config.allowUnfree = true;
 
-   # Catppuccin!
+  # Catppuccin!
   catppuccin.enable = true;
   catppuccin.flavor = "mocha";
 
   #Modules
   imports = [
-      ./ghostty.nix
-      ./helix.nix
-      ./fish.nix
-      ./flatpak.nix
-      ./kdeconnect.nix
-      ./starship.nix
-      ./git.nix
-      ./direnv.nix
-      ./yazi.nix
-      ];
+    ./ghostty.nix
+    ./helix.nix
+    ./fish.nix
+    ./flatpak.nix
+    ./kdeconnect.nix
+    ./starship.nix
+    ./git.nix
+    ./direnv.nix
+    ./yazi.nix
+    ./eza.nix
+    ./fzf.nix
+    ./atuin.nix
+  ];
 }
